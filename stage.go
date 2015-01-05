a652e50fa34731de967bb5f703c5f42552dd241e
Karrick McDermott <kmcdermott@linkedin.com>
2015-01-05 15:19:28 -0500

    allows custom busy and idle hooks
     * defaults to addition of one worker when a given stage is busy
     * defaults to removal of one worker when a given stage is idle

--- stage.go
+++ stage.go
@@ -8,8 +8,8 @@ import (
 )
 
 const (
-	workerIdleTimeout    = time.Minute / 4
-	defaultMaxToMinRatio = 16
+	defaultWorkerIdleTimeout = time.Minute / 4
+	defaultMaxToMinRatio     = 16
 )
 
 type stage struct {
@@ -20,7 +20,13 @@ type stage struct {
 	finished                                   chan<- struct{}
 	workerIdle, terminateWorker, workerExitted chan struct{}
 	workerCount, workerMin, workerMax          int
+	busyHook                                   BusyHook
+	idleHook                                   IdleHook
+	idleTimeout, busyTimeout                   time.Duration
 }
+
+type BusyHook func(min, count, max int) int
+type IdleHook func(min, count, max int) int
 
 // Stage is a job queue configuration function that appends a job
 // stage to the newly created job queue. The client specifies the name
@@ -33,18 +39,37 @@ func Stage(setters ...StageSetter) JobQueueSetter {
 		if q.input != nil {
 			return fmt.Errorf("stage cannot be created after queue")
 		}
-		stg := &stage{workerMin: 1}
+		stg := &stage{
+			workerMin:   1,
+			idleTimeout: defaultWorkerIdleTimeout,
+			busyTimeout: defaultWorkerIdleTimeout * 2,
+		}
 		for _, setter := range setters {
 			err := setter(stg)
 			if err != nil {
 				return err
 			}
 		}
+		if stg.busyTimeout <= stg.idleTimeout {
+			return fmt.Errorf("stage busy timeout must be larger than its idle timeout")
+		}
 		if stg.workerMax == 0 {
 			stg.workerMax = defaultMaxToMinRatio * stg.workerMin
 		}
 		if stg.workerMax < stg.workerMin {
 			return fmt.Errorf("stage minimum workers ought to be less than or equal to maximum workers")
+		}
+		if stg.busyHook == nil {
+			log.Printf("setting busy hook to default of add one worker when busy")
+			stg.busyHook = func(_, _, _ int) int {
+				return 1
+			}
+		}
+		if stg.idleHook == nil {
+			log.Printf("setting idle hook to default of remove one worker when idle")
+			stg.busyHook = func(_, _, _ int) int {
+				return 1
+			}
 		}
 		q.stages = append(q.stages, stg)
 		return nil
@@ -62,7 +87,6 @@ func stageMonitor(stg *stage) {
 
 	// spawn first workers
 	spawn(stg, stg.workerMin)
-	stg.workerCount = stg.workerMin
 
 monitorLoop:
 	for {
@@ -74,24 +98,34 @@ monitorLoop:
 				break monitorLoop
 			}
 		case <-stg.workerIdle:
-			if stg.workerCount > stg.workerMin {
-				// log.Print("[DEBUG] %s idle", stg.methodName)
-				stg.terminateWorker <- struct{}{}
-			} else {
-				log.Printf("[DEBUG] %s idle; at minimum number of workers: %d", stg.methodName, stg.workerMin)
+			log.Printf("[VERBOSE] %s idle", stg.methodName)
+			numberToRemove := stg.idleHook(stg.workerMin, stg.workerCount, stg.workerMax)
+			switch {
+			case numberToRemove > 0:
+				if stg.workerCount-numberToRemove < stg.workerMin {
+					log.Printf("[WARNING] %s idle; idle hook returned number which would cause worker count to fall below minimum: %d - %d < %d", stg.methodName, stg.workerCount, numberToRemove, stg.workerMin)
+					numberToRemove = stg.workerCount - stg.workerMin
+				}
+				for index := 0; index < numberToRemove; index++ {
+					stg.terminateWorker <- struct{}{}
+				}
+			case numberToRemove < 0:
+				log.Printf("[WARNING] %s idle; idle hook returned negative number: %d", stg.methodName, numberToRemove)
 			}
-		case <-time.After(workerIdleTimeout * 2):
-			// no workers idle after a minute; double what
-			// we have, not to exceed max
-			additional := stg.workerCount
-			if stg.workerCount+additional > stg.workerMax {
-				log.Printf("[DEBUG] %s busy; at maximum number of workers: %d", stg.methodName, stg.workerMax)
-				additional = stg.workerMax - stg.workerCount
-			}
-			if additional > 0 {
-				log.Printf("[DEBUG] %s busy", stg.methodName)
-				spawn(stg, additional)
-				stg.workerCount += additional
+		case <-time.After(stg.busyTimeout):
+			log.Printf("[VERBOSE] %s busy", stg.methodName)
+			numberToAdd := stg.busyHook(stg.workerMin, stg.workerCount, stg.workerMax)
+			switch {
+			case numberToAdd > 0:
+				if stg.workerCount+numberToAdd > stg.workerMax {
+					log.Printf("[WARNING] %s busy; busy hook returned number which would cause worker count to rise above maximum: %d + %d > %d", stg.methodName, stg.workerCount, numberToAdd, stg.workerMax)
+					numberToAdd = stg.workerMax - stg.workerCount
+				}
+				if numberToAdd > 0 {
+					spawn(stg, numberToAdd)
+				}
+			case numberToAdd < 0:
+				log.Printf("[WARNING] %s busy; busy hook returned negative number: %d", stg.methodName, numberToAdd)
 			}
 		case <-stg.terminate:
 			// log.Printf("[DEBUG] monitor terminate: %s", stg.methodName)
@@ -108,10 +142,11 @@ monitorLoop:
 }
 
 func spawn(stg *stage, count int) {
-	// log.Printf("[DEBUG] %s worker spawn: %d workers", stg.methodName, count)
+	log.Printf("[DEBUG] %s worker spawn: %d workers", stg.methodName, count)
 	for index := 0; index < count; index++ {
 		go worker(stg, stg.terminateWorker, stg.workerExitted)
 	}
+	stg.workerCount += count
 }
 
 func worker(stg *stage, terminate <-chan struct{}, finished chan<- struct{}) {
@@ -121,7 +156,6 @@ workerLoop:
 	for {
 		select {
 		case datum := <-input:
-			// log.Print("[DEBUG] worker job: ", stg.methodName)
 			datumType := reflect.TypeOf(datum)
 			method, ok := datumType.MethodByName(stg.methodName)
 			if !ok {
@@ -132,14 +166,12 @@ workerLoop:
 			method.Func.Call(values)
 			stg.output <- datum
 		case <-terminate:
-			// log.Print("[DEBUG] worker terminate: ", stg.methodName)
 			break workerLoop
-		case <-time.After(workerIdleTimeout):
-			// log.Print("[DEBUG] worker idle: ", stg.methodName)
+		case <-time.After(stg.idleTimeout):
 			stg.workerIdle <- struct{}{}
 		}
 	}
-	// log.Print("[DEBUG] worker finished: ", stg.methodName)
+	// log.Print("[VERBOSE] worker finished: ", stg.methodName)
 	finished <- struct{}{}
 }
 
@@ -188,6 +220,55 @@ func Min(count int) StageSetter {
 			return fmt.Errorf("ought to have at least one worker: %d", count)
 		}
 		stg.workerMin = count
+		return nil
+	}
+}
+
+// Static is a stage configuration function that specifies the static
+// number of workers a stage ought to have.
+func Static(count int) StageSetter {
+	return func(stg *stage) error {
+		if count <= 0 {
+			return fmt.Errorf("ought to have at least one worker: %d", count)
+		}
+		stg.workerMin = count
+		stg.workerMax = count
+		return nil
+	}
+}
+
+// Busy is a stage configuration function that specifies what BusyHook
+// function to invoke when workers are busy.
+func Busy(hook BusyHook) StageSetter {
+	return func(stg *stage) error {
+		stg.busyHook = hook
+		return nil
+	}
+}
+
+// Idle is a stage configuration function that specifies what IdleHook
+// function to invoke when workers are idle.
+func Idle(hook IdleHook) StageSetter {
+	return func(stg *stage) error {
+		stg.idleHook = hook
+		return nil
+	}
+}
+
+// BusyTimeout is a stage configuration function that specifies how
+// long before a stage monitor considers that stage busy.
+func BusyTimeout(t time.Duration) StageSetter {
+	return func(stg *stage) error {
+		stg.busyTimeout = t
+		return nil
+	}
+}
+
+// IdleTimeout is a stage configuration function that specifies how
+// long before a stage worker considers itself idle.
+func IdleTimeout(t time.Duration) StageSetter {
+	return func(stg *stage) error {
+		stg.idleTimeout = t
 		return nil
 	}
 }
