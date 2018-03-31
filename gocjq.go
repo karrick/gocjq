b74108aaccc8f4c1ec272ae393734672a464f37e
Karrick S. McDermott <karrick@karrick.net>
2018-03-31 00:42:19 -0400

    reflow comments

--- gocjq.go
+++ gocjq.go
@@ -1,24 +1,23 @@
-// It is often convenient to partition a job into multiple steps, and
-// have them run concurrently, like a car factory might have different
-// stages in a manufactoring facility.
+// It is often convenient to partition a job into multiple steps, and have them
+// run concurrently, like a car factory might have different stages in a
+// manufactoring facility.
 //
-// It is also convenient to specify how many workers are desired at each
-// stage of the process. For one stage you may need 5 workers, but for
-// another stage you may want between 25 and 50.
+// It is also convenient to specify how many workers are desired at each stage
+// of the process. For one stage you may need 5 workers, but for another stage
+// you may want between 25 and 50.
 //
-// This library, go concurent job queues, gocjq, makes setting up
-// these sort of examples quite easy. Create a structure to hold your
-// job's information, give your structure methods to be invoked for
-// each stage, then create a JobQueue that invokes your methods in the
-// proper order.
+// This library, go concurent job queues, gocjq, makes setting up these sort of
+// examples quite easy. Create a structure to hold your job's information, give
+// your structure methods to be invoked for each stage, then create a JobQueue
+// that invokes your methods in the proper order.
 package gocjq
 
 import (
 	"fmt"
 )
 
-// JobQueue allows enqueuing and dequeueing of jobs. The caller MUST
-// invoke Quit method when the queue is no longer needed.
+// JobQueue is the interface implemented by an object that allows processing of
+// jobs.  The caller MUST invoke Quit method when the queue is no longer needed.
 type JobQueue interface {
 	Input() chan<- interface{}
 	Output() <-chan interface{}
@@ -31,15 +30,13 @@ type jobQueue struct {
 	terminate, finished chan struct{}
 }
 
-// NewQueue creates a job queue. New jobs are enqueued by sending a
-// job to the channel returned by the queue's Input method. Jobs are
-// processed by the specified job stages, then sent to the queue's
-// output channel. If no output channel is specified, one will be
-// created by NewQueue. If no output channel is desired, the
-// OutputSump method is used to create a drain that will loop over the
-// queue's output channel after completion. Once the queue is no
-// longer needed, the client MUST call the Quit method to clean up the
-// channels.
+// NewQueue creates a job queue. New jobs are enqueued by sending a job to the
+// channel returned by the queue's Input method. Jobs are processed by the
+// specified job stages, then sent to the queue's output channel. If no output
+// channel is specified, one will be created by NewQueue. If no output channel
+// is desired, the OutputSump method is used to create a drain that will loop
+// over the queue's output channel after completion. Once the queue is no longer
+// needed, the client MUST call the Quit method to clean up the channels.
 //
 //  type someJob struct {
 //      a, b int
@@ -133,10 +130,10 @@ func (q *jobQueue) Output() <-chan interface{} {
 	return q.output
 }
 
-// Quit is called by the client once the queue is no longer needed to
-// clean up the channels. The Quit method closes the output channel,
-// including when the client specifies an output channel using the
-// Output method durng queue creation.
+// Quit is called by the client once the queue is no longer needed to clean up
+// the channels. The Quit method closes the output channel, including when the
+// client specifies an output channel using the Output method durng queue
+// creation.
 func (q *jobQueue) Quit() {
 	if len(q.stages) > 0 {
 		q.terminate <- struct{}{}
@@ -148,14 +145,14 @@ func (q *jobQueue) Quit() {
 	close(q.output)
 }
 
-// JobQueueSetter type defines functions that modify a newly created
-// job queue with various configuration settings.
+// JobQueueSetter type defines functions that modify a newly created job queue
+// with various configuration settings.
 type JobQueueSetter func(*jobQueue) error
 
-// Output is a job queue configuration function that allows the client
-// to specify a channel to send the completed jobs to. Failing to
-// receive jobs from this channel will have the effect of halting the
-// job queue once all the channels are full.
+// Output is a job queue configuration function that allows the client to
+// specify a channel to send the completed jobs to. Failing to receive jobs from
+// this channel will have the effect of halting the job queue once all the
+// channels are full.
 func Output(out chan interface{}) JobQueueSetter {
 	return func(q *jobQueue) error {
 		if q.output != nil {
@@ -169,11 +166,11 @@ func Output(out chan interface{}) JobQueueSetter {
 	}
 }
 
-// OutputSump is a job queue configuration function that creates a
-// queue that discards jobs after completetion. This is used when one
-// of the job stages performs a desired side effect. Because completed
-// jobs are discarded by the library, there is no need for client
-// removal of completed jobs from the job queue.
+// OutputSump is a job queue configuration function that creates a queue that
+// discards jobs after completetion. This is used when one of the job stages
+// performs a desired side effect. Because completed jobs are discarded by the
+// library, there is no need for client removal of completed jobs from the job
+// queue.
 //
 //  func main() {
 //      queue, err := gocjq.NewQueue(
